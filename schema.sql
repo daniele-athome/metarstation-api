07f4536d2c226ad13924b2f1af47b17e9eae7e97
Daniele Ricci <daniele@casaricci.it>
2026-01-17 19:28:01 +0100

    Prepare configuration for production deployment

--- schema.sql
+++ schema.sql
@@ -1,5 +1,8 @@
 CREATE TABLE measurements (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  timestamp TEXT NOT NULL UNIQUE,
+  timestamp TEXT NOT NULL,
   payload TEXT NOT NULL
 );
+
+CREATE UNIQUE INDEX measurements_timestamp_uindex
+	ON measurements (timestamp);
